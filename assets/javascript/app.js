0ea54c17dc32925b857753d964ad3be069f34acc
Max Reinmueller <maxreinmueller@gmail.com>
2019-07-25 11:23:03 -0700

    simplified javascript object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,68 @@
-// write a list of random trivia questions and store in an object array. use object for question, properties for possible answers.
+// selecting html elements
+var start = $('#start');
+var quiz = $('#quiz');
+var question = $('#question')
+var choiceA = $('#A')
+var choiceB = $('#B')
+var choiceC = $('#C')
+var choiceD = $('#D')
 
+// List of questions
+var questions = [
+    {
+    question: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
+    choiceA:'William and Elizabeth',
+    choiceB:'Joseph and Catherine',
+    choiceC:'John and Mary',
+    choiceD:'George and Anne',
+    correct: 'c',
+},
+{
+    question: "When did the Liberty Bell get its name?",
+    choiceA: 'when it was made, in 1701',
+    choiceB: 'when it rang on July 4, 1776',
+    choiceC: 'in the 19th century, when it became a symbol of the abolition of slavery',
+    choiceD: 'none of the above',
+    correct: 'c',
+},
+{
+    question: "In the Roy Rogers -Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy's horse was named Trigger, which was Dales horse?",
+    choiceA: 'Buttermilk',
+    choiceB: 'Daisy',
+    choiceC: 'Scout',
+    choiceD: 'Tulip',
+    correct: 'a',
+},
+{
+    question: "Which of the following items was owned by the fewest U.S. homes in 1990?",
+    choiceA: 'home computer',
+    choiceB: 'compact disk player',
+    choiceC:'cordless phone',
+    choiceD:'dishwasher',
+    correct: 'b',
+},
+{
+    question: "Who holds the record for the most victories in a row on the professional golf tour?",
+    choiceA: 'Jack Nicklaus',
+    choiceB: 'Arnold Palmer',
+    choiceC: 'Byron Nelson',
+    choiceD: 'Ben Hogan',
+    correct: 'c',
+},
+];
 
-$(document).ready(function(){
-    $("#hiddenAtFirst").addClass("hidden")
-});
-
-// importanat variables
+// variables for timer
 var number = 10;
 var intervalId;
 
-//  onClick event to start the game and executes run function
-    $('#start').on("click", run)
-
+//  Starts the timer
+$('#start').on("click", run)
 
 // sets an interval for the time
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-    // hides button
     $(this).hide();
-    // displays question div
     $("#hiddenAtFirst").show();
 }
 
@@ -41,14 +84,14 @@ function stop() {
 
 
 
-// timer starts counting down
+
 
 // question is displayed with possible answers
 
 // userpicks answer and is told if they are rigt or wrong
 
 // next question starts after 5 seconds of notifying user
- 
+
 // store the true or false in the object value
 // questions are randomly generated, 1 per object and displayed on the screen
 // track true and false answers for users score per object
